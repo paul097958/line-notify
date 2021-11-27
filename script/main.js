dc99392c718953ee189fe2f0809461dcbacf291c
letterpaulsand <letterpaulsand@gmail.com>
2021-11-28 01:10:16 +0800

    v1 add server

--- script/main.js
+++ script/main.js
@@ -3,10 +3,22 @@ const app = express();
 
 app.get('/', (req, res) => {
     try {
-        res.sendFile('C:/Users/paul/Desktop/line/src/index.html')
+        res.sendFile('C:/Users/paul/Desktop/line/src/index.html');
     } catch (e) {
         console.error('sever error', e);
         res.sendStatus(500)
 
     }
 })
+
+app.get('/index.js', (req, res) => {
+    try {
+        res.sendFile('C:/Users/paul/Desktop/line/src/js/index.js');
+    } catch (e) {
+        console.error('sever error', e);
+        res.sendStatus(500)
+
+    }
+})
+
+app.listen(3000)
