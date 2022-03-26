fc0079c5fc3e26e5db0d8c6abebb774d46b88ace
letterpaulsand <letterpaulsand@gmail.com>
2022-03-26 23:31:49 +0800

    test

--- script/main.js
+++ script/main.js
@@ -1,70 +1,15 @@
-const express = require('express');
+const express = require('express')
+
 const app = express();
-const axios = require('axios');
-const Formdata = require('form-data');
-const path = require('path');
-const port = 3000 || process.env.EXPRESS_PORT
-require('dotenv').config();
-const pathDir = path.resolve('./src');
-console.log('the src dir:', pathDir);
+const port = 3000;
+app.get("/", (req, res) => {
+  res.json({ message: "Hello From Express Ap" });
+});
 
-// send the index file
-app.get('/', (req, res) => {
-    try {
-        res.sendFile(pathDir+'/index.html');
-    } catch (e) {
-        console.error('sever error', e);
-        res.sendStatus(500)
+app.get('/hello', (req, res)=>{
+  res.send('heel')
+});
 
-    }
-})
-// load js file
-app.get('/index.js', (req, res) => {
-    try {
-        res.sendFile(pathDir+'/js/index.js');
-    } catch (e) {
-        console.error('sever error', e);
-        res.sendStatus(500)
-
-    }
-})
-//  send axios req
-app.get('/readform', (req, res) => {
-    try {
-        //  send index file
-        res.sendFile(pathDir+'/index.html');
-        //  get get req
-        let message = req.query.message;
-        let stickerPackageId = req.query.stickerPackageId;
-        let stickerId = req.query.stickerId;
-        //  make data
-        let formdata = new Formdata()
-        formdata.append("message", message);
-        formdata.append("stickerPackageId", stickerPackageId);
-        formdata.append("stickerId", stickerId);
-        //  make header
-        let head = Object.assign({
-            'Authorization': `Bearer ${process.env.LINE_TOKEN}`
-        }, formdata.getHeaders())
-        //  make axios server
-        axios({
-            method: "post",
-            url: 'https://notify-api.line.me/api/notify',
-            headers: head,
-            data: formdata
-
-        })
-            .then(function (res) {
-                console.log('axios res data:',res.data);
-            })
-            .catch(function (e) {
-                console.log('axios err:',e);
-            });
-    } catch (e) {
-        console.error('axios sever error:', e);
-        res.sendStatus(500)
-    }
-})
-
-app.listen(port);
-
+app.listen(9000, () => {
+  console.log(`Starting Server on Port ${port}`);
+});
